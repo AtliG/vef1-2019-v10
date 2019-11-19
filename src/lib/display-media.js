23f84868812651f8b78641f9283b20d59529ee97
Atli <atligudjonsson6@gmail.com>
2019-11-19 21:35:37 +0000

    Commiting changes

--- src/lib/display-media.js
+++ src/lib/display-media.js
@@ -1,4 +1,6 @@
 // todo vísa í rétta hluti með import
+import getRandomImage from './nasa-api';
+import { save, load } from './storage';
 
 // breytur til þess að halda utan um html element nodes
 let title; // titill fyrir mynd á forsíðu
@@ -11,15 +13,21 @@ let image; // object sem inniheldur núverandi mynd á forsíðu.
  * Sækir nýja Mynd af handahófi frá Nasa API og birtir hana á forsíðunni
  * ásamt titli og texta.
  */
-function getNewImage() {
-
+async function getNewImage() {
+  image = await getRandomImage();
+  console.log(image);
+  console.log('Hello');
+  text.innerText = image.text;
+  title.innerText = image.title;
+  img.src = image.mediaUrl;
 }
 
 /*
  * Vistar núverandi mynd í storage.
  */
 function saveCurrentImage() {
-
+  console.log('I work!');
+  save(image.type, image.mediaUrl, image.text, image.title);
 }
 
 /*
@@ -27,7 +35,13 @@ function saveCurrentImage() {
  *
  */
 export default function init(apod) {
+  text = document.querySelector('.apod__text');
+  title = document.querySelector('.apod__title');
+  img = document.querySelector('.apod__image');
 
+  document.querySelector('#new-image-button').addEventListener('click', getNewImage);
+  document.querySelector('#save-image-button').addEventListener('click', saveCurrentImage);
+  getNewImage();
 }
 
 /*
@@ -35,5 +49,22 @@ export default function init(apod) {
  * titlum þeirra.
  */
 export function loadFavourites() {
+  const main = document.querySelector('main');
+  console.log('Vistaðar myndir comin up');
+  var storedImages = load();
+  storedImages.forEach((obj) => {
+    const titleElement = document.createElement('h1');
+    titleElement.innerText = obj.title;
 
+    const imgElement = document.createElement('img');
+    imgElement.src = obj.mediaUrl;
+    imgElement.classList.add('apod__image');
+
+    const item = document.createElement('div');
+    item.classList.add('apod');
+    item.appendChild(titleElement);
+    item.appendChild(imgElement);
+
+    main.appendChild(item);
+  });
 }
--- src/lib/nasa-api.js
+++ src/lib/nasa-api.js
@@ -1,4 +1,4 @@
-
+import { randomNumber } from './helpers';
 /**
  * Sækir Myndir frá nasa API. Til þess að sjá dæmi um json svari sjá apod.json
  */
@@ -6,8 +6,7 @@
 // API lykill til að fá aðgang að nasa gögnum.
 const API_KEY = 'DEMO_KEY';
 // Slóð að sækja myndir frá. Dæmi um heila slóð https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2019-11-10
-const URL = 'https://api.nasa.gov/planetary/apod';
-
+const URL = '//api.nasa.gov/planetary/apod?api_key=iXTV4P0XTkiL3BE3A7fRHQ7F8m7HCrbXZGpTzO70&date=';
 
 /**
  * Sækir mynd af handahófi frá APOD API hjá nasa
@@ -15,5 +14,31 @@ const URL = 'https://api.nasa.gov/planetary/apod';
  * @returns {Promise} sem mun innihalda upplýsingar um mynd/myndband hjá nasa.
  */
 export default async function getRandomImage() {
+  var randomDate = getRandomDate();
+  const searchString = URL + randomDate;
 
+  const res = await fetch(searchString);
+  const{
+    media_type: type,
+    url: mediaUrl,
+    explanation: text,
+    title,
+  } = await res.json();
+
+  return {
+    type, mediaUrl, text, title,
+  };
+}
+
+function getRandomDate() {
+  var fDate = new Date('June 16, 1995 00:00:00');
+  var lowerTime = fDate.getTime();
+  var currentTime = Date.now();
+  var time = randomNumber(lowerTime,currentTime);
+  var rDate = new Date(time);
+  var d = rDate.getDate();
+  var m = rDate.getMonth();
+  var y = rDate.getFullYear();
+
+  return `${y}-${m}-${d}`;
 }
